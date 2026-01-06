0e8f6641d21f4a1d9709d8e337ae9fe9fec0aefe
Srivardhan <srivardhan.v@aifalabs.com>
2026-01-06 15:48:35 +0530

    task completed sucessfully

---
+++ Modular_CLI_Task_Manager/target/debug/deps/Modular_CLI_Task_Manager-2f03ef348203ff37.d
@@ -0,0 +1,9 @@
+/home/aifauser139/rust/rust-github/Modular_CLI_Task_Manager/target/debug/deps/Modular_CLI_Task_Manager-2f03ef348203ff37.d: src/main.rs Cargo.toml
+
+/home/aifauser139/rust/rust-github/Modular_CLI_Task_Manager/target/debug/deps/libModular_CLI_Task_Manager-2f03ef348203ff37.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ Modular_CLI_Task_Manager/target/debug/deps/Modular_CLI_Task_Manager-e927b5f4be80cb8b.d
@@ -0,0 +1,14 @@
+/home/aifauser139/rust/rust-github/Modular_CLI_Task_Manager/target/debug/deps/Modular_CLI_Task_Manager-e927b5f4be80cb8b.d: src/lib.rs src/error.rs src/storage/mod.rs src/storage/memory.rs src/task/mod.rs src/task/model.rs Cargo.toml
+
+/home/aifauser139/rust/rust-github/Modular_CLI_Task_Manager/target/debug/deps/libModular_CLI_Task_Manager-e927b5f4be80cb8b.rmeta: src/lib.rs src/error.rs src/storage/mod.rs src/storage/memory.rs src/task/mod.rs src/task/model.rs Cargo.toml
+
+src/lib.rs:
+src/error.rs:
+src/storage/mod.rs:
+src/storage/memory.rs:
+src/task/mod.rs:
+src/task/model.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
