d12e4ed894c46d9e30848d29ec719cca799487fa
Srivardhan <srivardhan.v@aifalabs.com>
2026-01-08 12:08:36 +0530

    exporting the files in a new format

--- Generic_Notification_Processing_Engine/target/debug/deps/Generic_Notification_Processing_Engine-bb6c9c3bfa8a9876.d
+++ Generic_Notification_Processing_Engine/target/debug/deps/Generic_Notification_Processing_Engine-bb6c9c3bfa8a9876.d
@@ -1,10 +1,10 @@
-/home/aifauser139/rust/rust-github/Generic_Notification_Processing_Engine/target/debug/deps/Generic_Notification_Processing_Engine-bb6c9c3bfa8a9876.d: src/main.rs src/notifier.rs src/providers/mod.rs src/providers/email.rs src/providers/push.rs src/providers/sms.rs src/service.rs
+/home/aifauser139/rust/rust-github/Generic_Notification_Processing_Engine/target/debug/deps/Generic_Notification_Processing_Engine-bb6c9c3bfa8a9876.d: src/main.rs src/notifier.rs src/providers.rs src/providers/email.rs src/providers/push.rs src/providers/sms.rs src/service.rs
 
-/home/aifauser139/rust/rust-github/Generic_Notification_Processing_Engine/target/debug/deps/libGeneric_Notification_Processing_Engine-bb6c9c3bfa8a9876.rmeta: src/main.rs src/notifier.rs src/providers/mod.rs src/providers/email.rs src/providers/push.rs src/providers/sms.rs src/service.rs
+/home/aifauser139/rust/rust-github/Generic_Notification_Processing_Engine/target/debug/deps/libGeneric_Notification_Processing_Engine-bb6c9c3bfa8a9876.rmeta: src/main.rs src/notifier.rs src/providers.rs src/providers/email.rs src/providers/push.rs src/providers/sms.rs src/service.rs
 
 src/main.rs:
 src/notifier.rs:
-src/providers/mod.rs:
+src/providers.rs:
 src/providers/email.rs:
 src/providers/push.rs:
 src/providers/sms.rs:
--- Generic_Notification_Processing_Engine/target/debug/deps/Generic_Notification_Processing_Engine-e79430b40bf25f20.d
+++ Generic_Notification_Processing_Engine/target/debug/deps/Generic_Notification_Processing_Engine-e79430b40bf25f20.d
@@ -1,10 +1,10 @@
-/home/aifauser139/rust/rust-github/Generic_Notification_Processing_Engine/target/debug/deps/Generic_Notification_Processing_Engine-e79430b40bf25f20.d: src/main.rs src/notifier.rs src/providers/mod.rs src/providers/email.rs src/providers/push.rs src/providers/sms.rs src/service.rs
+/home/aifauser139/rust/rust-github/Generic_Notification_Processing_Engine/target/debug/deps/Generic_Notification_Processing_Engine-e79430b40bf25f20.d: src/main.rs src/notifier.rs src/providers.rs src/providers/email.rs src/providers/push.rs src/providers/sms.rs src/service.rs
 
-/home/aifauser139/rust/rust-github/Generic_Notification_Processing_Engine/target/debug/deps/libGeneric_Notification_Processing_Engine-e79430b40bf25f20.rmeta: src/main.rs src/notifier.rs src/providers/mod.rs src/providers/email.rs src/providers/push.rs src/providers/sms.rs src/service.rs
+/home/aifauser139/rust/rust-github/Generic_Notification_Processing_Engine/target/debug/deps/libGeneric_Notification_Processing_Engine-e79430b40bf25f20.rmeta: src/main.rs src/notifier.rs src/providers.rs src/providers/email.rs src/providers/push.rs src/providers/sms.rs src/service.rs
 
 src/main.rs:
 src/notifier.rs:
-src/providers/mod.rs:
+src/providers.rs:
 src/providers/email.rs:
 src/providers/push.rs:
 src/providers/sms.rs:
